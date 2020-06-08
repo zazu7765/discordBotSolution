bf473fc6832162ca36edba01b000391e08d612a4
Daniel <ohmyzazu@gmail.com>
2020-06-08 08:28:14 -0400

    Setup

---
+++ discordBotProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discordBotProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
