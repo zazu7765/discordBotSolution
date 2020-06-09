a6981e70460425b507f89e207b8f00249494c7e1
Daniel <ohmyzazu@gmail.com>
2020-06-09 08:51:04 -0400

    First part's done!

--- discordBotProject/Program.cs
+++ discordBotProject/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -7,9 +7,31 @@ namespace discordBotProject
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+           _client = new DiscordSocketClient();
+           _client.Log += log;
+           _client.MessageReceived += MessageReceivedTask;
+           await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+           await _client.StartAsync();
+           await Task.Delay(-1);
+        }
+        private Task log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        private async Task MessageReceivedTask(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                Console.Write("yoohoo");
+                await message.Channel.SendMessageAsync("Pong!");
+            }
         }
     }
 }
